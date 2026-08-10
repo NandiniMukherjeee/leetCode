f4f6e1e53aba3a920e0a927ffe4faa51fc7ff365
Nandini Mukherjee <158547260+NandiniMukherjeee@users.noreply.github.com>
2026-08-10 23:37:11 +0530

    Time: 691 ms (45.76%), Space: 20 MB (85.15%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -0,0 +1,19 @@
+class Solution:
+    def winnerSquareGame(self, n):
+        dp = [False] * (n + 1)
+
+        for i in range(1, n + 1):
+
+            j = 1
+
+            while j * j <= i:
+
+                # Remove j*j stones
+                # If opponent loses, current player wins
+                if dp[i - j * j] == False:
+                    dp[i] = True
+                    break
+
+                j += 1
+
+        return dp[n]
